1abe38fa58782ba42b4b4c331a5c5577c855230e
mandy <hitangman@gmail.com>
2017-12-12 13:55:56 +0800

    feat(Route): add default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: 'heroes', component: HeroesComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', component: DashboardComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
