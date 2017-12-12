61b440554b372504240f7684153a10648f92b5cf
mandy <hitangman@gmail.com>
2017-12-12 14:41:04 +0800

    feat(HeroService): add getHero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,10 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+  getHero(id: Number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero with id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
   constructor(private messageService: MessageService) { }
 
 }
