0558648331aec0c220c30db40657de9fe1107f37
mandy <hitangman@gmail.com>
2017-12-12 14:06:22 +0800

    fix(Dashbaord): excute getHeroed in lifecircle hook

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,11 +10,14 @@ import { HeroService } from '../hero.service';
 export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe( heroes => this.heroes = heroes.slice(1, 5));
+  }
   constructor(private heroService: HeroService) {
-    heroService.getHeroes().subscribe( heroes => this.heroes = heroes.slice(1, 5));
   }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
