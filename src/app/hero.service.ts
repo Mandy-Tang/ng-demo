8dfb9a6893df071269c5341cb32eefc9c7e86646
mandy <hitangman@gmail.com>
2017-12-20 15:48:29 +0800

    feat(HeroService): errorhandler for getHeroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { catchError, tap} from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -15,10 +16,20 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
   getHeroes(): Observable<Hero[]> {
-    console.log('get');
-    this.log('HeroService: fetched heroes');
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes => this.log(`fetched heroes`)),
+        catchError(this.handleError('getHeroes', []))
+      );
   }
 
   getHero(id: Number): Observable<Hero> {
