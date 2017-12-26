1661c1045d7a4cea4454e99202551d190f0d9d81
mandy <hitangman@gmail.com>
2017-12-26 20:20:21 +0800

    feat(HeroSearch): search feature in HeroSearch component

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs/Observable';
+
+import { Hero } from '../hero';
+import { Subject } from 'rxjs/Subject';
+import { debounceTime } from 'rxjs/operators/debounceTime';
+import { distinctUntilChanged } from 'rxjs/operators/distinctUntilChanged';
+import { switchMap } from 'rxjs/operators/switchMap';
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +14,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
-
-  constructor() { }
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+  searchHero (term: string) {
+    this.searchTerms.next(term);
+  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
   }
 
 }
